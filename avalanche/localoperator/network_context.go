9708b60abd0a419157d07f851698cfc6995990ba
Felipe Madero <felipe.madero@gmail.com>
2021-10-22 21:26:26 -0300

    add missing file

---
+++ avalanche/localoperator/network_context.go
@@ -0,0 +1,14 @@
+package localoperator
+
+type NetworkContext struct {
+    network *Network
+}
+
+func NewNetworkContext(network *Network) *NetworkContext {
+    return &NetworkContext{network}
+}
+
+func (nc *NetworkContext) Fatal(err error) {
+    nc.network.Stop()
+    panic(err)
+}
