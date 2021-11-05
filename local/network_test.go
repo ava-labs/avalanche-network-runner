3b1b499c688103dca38420f90a893924b7eb94a4
Felipe Madero <felipe.madero@gmail.com>
2021-11-05 20:46:29 -0300

    use assert in all tests

--- local/network_test.go
+++ local/network_test.go
@@ -5,8 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 	"testing"
 
 	"github.com/ava-labs/avalanche-network-runner-local/api"
@@ -17,10 +15,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var _ NewNodeProcessF = newFullMockProcess
-var _ NewNodeProcessF = newMockProcessStartFail
+var _ NewNodeProcessF = newMockProcessUndef
+var _ NewNodeProcessF = newMockProcessSuccessful
+var _ NewNodeProcessF = newMockProcessFailedStart
 
-func newFullMockProcess(node.Config, ...string) (NodeProcess, error) {
+func newMockProcessUndef(node.Config, ...string) (NodeProcess, error) {
+	return &mocks.NodeProcess{}, nil
+}
+
+func newMockProcessSuccessful(node.Config, ...string) (NodeProcess, error) {
 	process := &mocks.NodeProcess{}
 	process.On("Start").Return(nil)
 	process.On("Wait").Return(nil)
@@ -28,7 +31,7 @@ func newFullMockProcess(node.Config, ...string) (NodeProcess, error) {
 	return process, nil
 }
 
-func newMockProcessStartFail(node.Config, ...string) (NodeProcess, error) {
+func newMockProcessFailedStart(node.Config, ...string) (NodeProcess, error) {
 	process := &mocks.NodeProcess{}
 	process.On("Start").Return(errors.New("Start failed"))
 	process.On("Wait").Return(nil)
@@ -36,7 +39,80 @@ func newMockProcessStartFail(node.Config, ...string) (NodeProcess, error) {
 	return process, nil
 }
 
+func TestNewNetworkEmpty(t *testing.T) {
+	assert := assert.New(t)
+	config := network.Config{
+		NodeConfigs: nil,
+		LogLevel:    "DEBUG",
+		Name:        "My Network",
+	}
+	net, err := NewNetwork(
+		logging.NoLog{},
+		config,
+		api.NewAPIClient, // TODO change AvalancheGo so we can mock API clients
+		newMockProcessUndef,
+	)
+	assert.NoError(err)
+	// Assert that GetNodesNames() includes only the 1 node's name
+	names, err := net.GetNodesNames()
+	assert.NoError(err)
+	assert.Len(names, 0)
+}
+
+// Start a network with one node.
+func TestNewNetworkOneNode(t *testing.T) {
+	assert := assert.New(t)
+	binaryPath := "yeet"
+	nodeName := "Bob"
+	// TODO remove test files when we can auto-generate genesis
+	// and other files
+	genesis, err := os.ReadFile("test_files/test_genesis.json")
+	assert.NoError(err)
+	avalancheGoConfig, err := os.ReadFile("test_files/config.json")
+	assert.NoError(err)
+	nodeConfig := node.Config{
+		ImplSpecificConfig: NodeConfig{
+			BinaryPath: binaryPath,
+		},
+		ConfigFile:  avalancheGoConfig,
+		Name:        nodeName,
+		IsBeacon:    true,
+		GenesisFile: genesis,
+	}
+	config := network.Config{
+		NodeCount:   1,
+		NodeConfigs: []node.Config{nodeConfig},
+		LogLevel:    "DEBUG",
+		Name:        "My Network",
+	}
+	// Assert that the node's config is being passed correctly
+	// to the function that starts the node process.
+	newProcessF := func(config node.Config, _ ...string) (NodeProcess, error) {
+		assert.EqualValues(nodeName, config.Name)
+		assert.True(config.IsBeacon)
+		assert.EqualValues(genesis, config.GenesisFile)
+		assert.EqualValues(avalancheGoConfig, config.ConfigFile)
+		assert.EqualValues(binaryPath, config.ImplSpecificConfig.(NodeConfig).BinaryPath)
+		process := &mocks.NodeProcess{}
+		process.On("Start").Return(nil)
+		return process, nil
+	}
+	net, err := NewNetwork(
+		logging.NoLog{},
+		config,
+		api.NewAPIClient,
+		newProcessF,
+	)
+	assert.NoError(err)
+	// Assert that GetNodesNames() includes only the 1 node's name
+	names, err := net.GetNodesNames()
+	assert.NoError(err)
+	assert.Contains(names, nodeName)
+	assert.Len(names, 1)
+}
+
 func TestWrongNetworkConfigs(t *testing.T) {
+	assert := assert.New(t)
 	networkConfigs := []network.Config{
 		// no ImplSpecificConfig
 		{
@@ -173,111 +249,66 @@ func TestWrongNetworkConfigs(t *testing.T) {
 		},
 	}
 	for _, networkConfig := range networkConfigs {
-		_, err := startNetwork(t, &networkConfig)
-		assert.Error(t, err)
+		_, err := NewNetwork(logging.NoLog{}, networkConfig, api.NewAPIClient, newMockProcessSuccessful)
+		assert.Error(err)
 	}
 }
 
 func TestImplSpecificConfigInterface(t *testing.T) {
+	assert := assert.New(t)
 	networkConfig, err := GetNetworkConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-	networkConfig.NodeConfigs[0].ImplSpecificConfig = "pepito"
-	_, err = startNetwork(t, networkConfig)
-	if err == nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
+	networkConfig.NodeConfigs[0].ImplSpecificConfig = "should not be string"
+	_, err = NewNetwork(logging.NoLog{}, networkConfig, api.NewAPIClient, newMockProcessSuccessful)
+	assert.Error(err)
 }
 
-func TestInvalidCommand(t *testing.T) {
-	networkConfig, err := GetNetworkConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = NewNetwork(logging.NoLog{}, *networkConfig, api.NewAPIClient, newMockProcessStartFail)
-	if err == nil {
-		t.Fatal(err)
-	}
-}
-
-/*
-Needs fake health api?
 func TestUnhealthyNetwork(t *testing.T) {
+	t.Skip()
+	assert := assert.New(t)
 	networkConfig, err := GetNetworkConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-	networkConfig.NodeConfigs[0].ConfigFile = []byte(`{"network-id":1}`)
-	net, err := startNetwork(t, networkConfig)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		ctx := context.TODO()
-		_ = net.Stop(ctx)
-	}()
-	if awaitNetwork(net) == nil {
-		t.Fatal(errors.New("expected network to never get healthy, but it was"))
-	}
+	assert.NoError(err)
+	_, err = NewNetwork(logging.NoLog{}, networkConfig, api.NewAPIClient, newMockProcessSuccessful)
+	assert.NoError(err)
+	// TODO: needs to set fake fail health api
+	//assert.Error(awaitNetwork(net))
 }
-*/
 
 func TestGeneratedNodesNames(t *testing.T) {
+	assert := assert.New(t)
 	networkConfig, err := GetNetworkConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
 	for i := range networkConfig.NodeConfigs {
 		networkConfig.NodeConfigs[i].Name = ""
 	}
-	net, err := startNetwork(t, networkConfig)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		ctx := context.TODO()
-		_ = net.Stop(ctx)
-	}()
+	net, err := NewNetwork(logging.NoLog{}, networkConfig, api.NewAPIClient, newMockProcessSuccessful)
+	assert.NoError(err)
 	nodeNameMap := make(map[string]bool)
 	nodeNames, err := net.GetNodesNames()
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
 	for _, nodeName := range nodeNames {
 		nodeNameMap[nodeName] = true
 	}
-	if len(nodeNameMap) != len(networkConfig.NodeConfigs) {
-		t.Fatalf("number of unique node names in network %v differs from number of nodes in config %v", len(nodeNameMap), len(networkConfig.NodeConfigs))
-	}
+	assert.EqualValues(len(nodeNameMap), len(networkConfig.NodeConfigs))
 }
 
 // TODO add byzantine node to conf
 // TestNetworkFromConfig creates/waits/checks/stops a network from config file
 // the check verify that all the nodes api clients are up
 func TestNetworkFromConfig(t *testing.T) {
+	assert := assert.New(t)
 	networkConfig, err := GetNetworkConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-	net, err := startNetwork(t, networkConfig)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		ctx := context.TODO()
-		_ = net.Stop(ctx)
-	}()
-	if err := awaitNetwork(net); err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
+	net, err := NewNetwork(logging.NoLog{}, networkConfig, api.NewAPIClient, newMockProcessSuccessful)
+	assert.NoError(err)
+	// TODO: needs to set fake successful health api
+	//assert.NoError(awaitNetwork(net))
 	runningNodes := make(map[string]bool)
 	for _, nodeConfig := range networkConfig.NodeConfigs {
 		runningNodes[nodeConfig.Name] = true
 	}
-	if err := checkNetwork(t, net, runningNodes, nil); err != nil {
-		t.Fatal(err)
-	}
+	// TODO: needs to set fake successful info api
+	checkNetwork(t, net, runningNodes, nil)
 }
 
 // TestNetworkNodeOps creates/waits/checks/stops a network created from an empty one
@@ -285,240 +316,131 @@ func TestNetworkFromConfig(t *testing.T) {
 // the check verify that all the nodes api clients are up for started nodes, and down for removed nodes
 // all nodes are taken from config file
 func TestNetworkNodeOps(t *testing.T) {
+	// TODO: needs to set fake successful info api for running nodes and failed info api for removed nodes
+	assert := assert.New(t)
 	networkConfig, err := GetNetworkConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-	net, err := startNetwork(t, &network.Config{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		ctx := context.TODO()
-		_ = net.Stop(ctx)
-	}()
+	assert.NoError(err)
+	net, err := NewNetwork(logging.NoLog{}, network.Config{}, api.NewAPIClient, newMockProcessSuccessful)
+	assert.NoError(err)
 	runningNodes := make(map[string]bool)
 	for _, nodeConfig := range networkConfig.NodeConfigs {
 		_, err = net.AddNode(nodeConfig)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(err)
 		runningNodes[nodeConfig.Name] = true
-		if err := checkNetwork(t, net, runningNodes, nil); err != nil {
-			t.Fatal(err)
-		}
+		checkNetwork(t, net, runningNodes, nil)
 	}
-	if err := awaitNetwork(net); err != nil {
-		t.Fatal(err)
-	}
+	// TODO: needs to set fake successful health api
+	//assert.NoError(awaitNetwork(net))
 	var removedClients []api.Client
 	for _, nodeConfig := range networkConfig.NodeConfigs {
 		node, err := net.GetNode(nodeConfig.Name)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(err)
 		client := node.GetAPIClient()
 		removedClients = append(removedClients, client)
 		err = net.RemoveNode(nodeConfig.Name)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(err)
 		delete(runningNodes, nodeConfig.Name)
-		if err := checkNetwork(t, net, runningNodes, removedClients); err != nil {
-			t.Fatal(err)
-		}
+		checkNetwork(t, net, runningNodes, removedClients)
 	}
 }
 
 // TestNodeNotFound checks operations fail for unkown node
 func TestNodeNotFound(t *testing.T) {
+	assert := assert.New(t)
 	networkConfig, err := GetNetworkConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-	net, err := startNetwork(t, &network.Config{})
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
+	net, err := NewNetwork(logging.NoLog{}, network.Config{}, api.NewAPIClient, newMockProcessSuccessful)
+	assert.NoError(err)
 	_, err = net.AddNode(networkConfig.NodeConfigs[0])
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
 	// get correct node
 	_, err = net.GetNode(networkConfig.NodeConfigs[0].Name)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
 	// get uncorrect node (non created)
 	_, err = net.GetNode(networkConfig.NodeConfigs[1].Name)
-	if err == nil {
-		t.Fatal(err)
-	}
+	assert.Error(err)
 	// remove uncorrect node (non created)
 	err = net.RemoveNode(networkConfig.NodeConfigs[1].Name)
-	if err == nil {
-		t.Fatal(err)
-	}
+	assert.Error(err)
 	// remove correct node
 	err = net.RemoveNode(networkConfig.NodeConfigs[0].Name)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
 	// get uncorrect node (removed)
 	_, err = net.GetNode(networkConfig.NodeConfigs[0].Name)
-	if err == nil {
-		t.Fatal(err)
-	}
+	assert.Error(err)
 	// remove uncorrect node (removed)
 	err = net.RemoveNode(networkConfig.NodeConfigs[0].Name)
-	if err == nil {
-		t.Fatal(err)
-	}
-	ctx := context.TODO()
-	err = net.Stop(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Error(err)
 }
 
 // TestStoppedNetwork checks operations fail for an already stopped network
 func TestStoppedNetwork(t *testing.T) {
+	assert := assert.New(t)
 	networkConfig, err := GetNetworkConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-	net, err := startNetwork(t, &network.Config{})
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
+	net, err := NewNetwork(logging.NoLog{}, network.Config{}, api.NewAPIClient, newMockProcessSuccessful)
+	assert.NoError(err)
 	_, err = net.AddNode(networkConfig.NodeConfigs[0])
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
 	// first GetNodesNames should return some nodes
 	_, err = net.GetNodesNames()
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
 	ctx := context.TODO()
 	err = net.Stop(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(err)
 	// Stop failure
-	err = net.Stop(ctx)
-	if err == nil {
-		t.Fatal(err)
-	}
+	assert.EqualValues(net.Stop(ctx), errStopped)
 	// AddNode failure
 	_, err = net.AddNode(networkConfig.NodeConfigs[1])
-	if err != errStopped {
-		t.Fatal(err)
-	}
+	assert.EqualValues(err, errStopped)
 	// GetNode failure
 	_, err = net.GetNode(networkConfig.NodeConfigs[0].Name)
-	if err != errStopped {
-		t.Fatal(err)
-	}
+	assert.EqualValues(err, errStopped)
 	// second GetNodesNames should return no nodes
 	_, err = net.GetNodesNames()
-	if err != errStopped {
-		t.Fatal(err)
-	}
+	assert.EqualValues(err, errStopped)
 	// RemoveNode failure
-	err = net.RemoveNode(networkConfig.NodeConfigs[0].Name)
-	if err != errStopped {
-		t.Fatal(err)
-	}
+	assert.EqualValues(net.RemoveNode(networkConfig.NodeConfigs[0].Name), errStopped)
 	// Healthy failure
-	healthyCh := net.Healthy()
-	err, ok := <-healthyCh
-	if !ok {
-		t.Fatal(errors.New("should return error"))
-	}
-	//err = awaitNetwork(net)
-	if err != errStopped {
-		t.Fatal(err)
-	}
+	assert.EqualValues(awaitNetwork(net), errStopped)
 }
 
-func startNetwork(t *testing.T, networkConfig *network.Config) (network.Network, error) {
-	net, err := NewNetwork(logging.NoLog{}, *networkConfig, api.NewAPIClient, newFullMockProcess)
-	if err != nil {
-		return nil, err
-	}
-	signalsCh := make(chan os.Signal, 1)
-	signal.Notify(signalsCh, syscall.SIGINT)
-	signal.Notify(signalsCh, syscall.SIGTERM)
-	go func() {
-		<-signalsCh
-		ctx := context.TODO()
-		_ = net.Stop(ctx)
-	}()
-	return net, nil
-}
-
-func checkNetwork(t *testing.T, net network.Network, runningNodes map[string]bool, removedClients []api.Client) error {
+func checkNetwork(t *testing.T, net network.Network, runningNodes map[string]bool, removedClients []api.Client) {
+	assert := assert.New(t)
 	nodeNames, err := net.GetNodesNames()
-	if err != nil {
-		return err
-	}
-	if len(nodeNames) != len(runningNodes) {
-		return fmt.Errorf("GetNodesNames() len %v should equal number of running nodes %v", len(nodeNames), len(runningNodes))
-	}
+	assert.NoError(err)
+	assert.EqualValues(len(nodeNames), len(runningNodes))
 	for nodeName := range runningNodes {
 		_, err := net.GetNode(nodeName)
-		if err != nil {
-			return err
-		}
+		assert.NoError(err)
 		//client := node.GetAPIClient()
-		//if _, err := client.InfoAPI().GetNodeID(); err != nil {
-		//	return err
-		//}
+		//assert.NoError(client.InfoAPI().GetNodeID())
 	}
 	//for _, client := range removedClients {
 	//	nodeID, err := client.InfoAPI().GetNodeID()
-	//	if err == nil {
-	//		return fmt.Errorf("removed node %v is answering requests", nodeID)
-	//	}
+	//  assert.Error(err)
 	//}
-	return nil
 }
 
 func awaitNetwork(net network.Network) error {
-	/*
-		timeoutCh := make(chan struct{})
-		go func() {
-			time.Sleep(5 * time.Minute)
-			timeoutCh <- struct{}{}
-		}()
-		healthyCh := net.Healthy()
-		select {
-		case err, ok := <-healthyCh:
-			if ok {
-				ctx := context.TODO()
-				_ = net.Stop(ctx)
-				return err
-			}
-		case <-timeoutCh:
-			ctx := context.TODO()
-			_ = net.Stop(ctx)
-			return errors.New("network startup timeout")
-		}
-	*/
+	healthyCh := net.Healthy()
+	err, ok := <-healthyCh
+	if ok {
+		return err
+	}
 	return nil
 }
 
-func GetNetworkConfig() (*network.Config, error) {
+func GetNetworkConfig() (network.Config, error) {
+	networkConfig := network.Config{}
 	genesisFile, err := os.ReadFile("test_files/network1/genesis.json")
 	if err != nil {
-		return nil, err
+		return networkConfig, err
 	}
 	cchainConfigFile, err := os.ReadFile("test_files/network1/cchain_config.json")
 	if err != nil {
-		return nil, err
+		return networkConfig, err
 	}
-	networkConfig := network.Config{}
 	for i := 1; i <= 6; i++ {
 		nodeConfig := node.Config{}
 		nodeConfig.GenesisFile = genesisFile
@@ -526,7 +448,7 @@ func GetNetworkConfig() (*network.Config, error) {
 		nodeConfig.Name = fmt.Sprintf("node%d", i)
 		configFile, err := os.ReadFile(fmt.Sprintf("test_files/network1/node%d/config.json", i))
 		if err != nil {
-			return nil, err
+			return networkConfig, err
 		}
 		nodeConfig.ConfigFile = configFile
 		certFile, err := os.ReadFile(fmt.Sprintf("test_files/network1/node%d/staking.crt", i))
@@ -545,83 +467,5 @@ func GetNetworkConfig() (*network.Config, error) {
 		networkConfig.NodeConfigs = append(networkConfig.NodeConfigs, nodeConfig)
 	}
 	networkConfig.NodeConfigs[0].IsBeacon = true
-	return &networkConfig, nil
-}
-
-var _ NewNodeProcessF = newMockProcess
-
-func newMockProcess(node.Config, ...string) (NodeProcess, error) {
-	return &mocks.NodeProcess{}, nil
-}
-
-func TestNewNetworkEmpty(t *testing.T) {
-	assert := assert.New(t)
-	config := network.Config{
-		NodeConfigs: nil,
-		LogLevel:    "DEBUG",
-		Name:        "My Network",
-	}
-	net, err := NewNetwork(
-		logging.NoLog{},
-		config,
-		api.NewAPIClient, // TODO change AvalancheGo so we can mock API clients
-		newMockProcess,
-	)
-	assert.NoError(err)
-	// Assert that GetNodesNames() includes only the 1 node's name
-	names, err := net.GetNodesNames()
-	assert.NoError(err)
-	assert.Len(names, 0)
-}
-
-// Start a network with one node.
-func TestNewNetworkOneNode(t *testing.T) {
-	assert := assert.New(t)
-	binaryPath := "yeet"
-	nodeName := "Bob"
-	// TODO remove test files when we can auto-generate genesis
-	// and other files
-	genesis, err := os.ReadFile("test_files/test_genesis.json")
-	assert.NoError(err)
-	avalancheGoConfig, err := os.ReadFile("test_files/config.json")
-	assert.NoError(err)
-	nodeConfig := node.Config{
-		ImplSpecificConfig: NodeConfig{
-			BinaryPath: binaryPath,
-		},
-		ConfigFile:  avalancheGoConfig,
-		Name:        nodeName,
-		IsBeacon:    true,
-		GenesisFile: genesis,
-	}
-	config := network.Config{
-		NodeCount:   1,
-		NodeConfigs: []node.Config{nodeConfig},
-		LogLevel:    "DEBUG",
-		Name:        "My Network",
-	}
-	// Assert that the node's config is being passed correctly
-	// to the function that starts the node process.
-	newProcessF := func(config node.Config, _ ...string) (NodeProcess, error) {
-		assert.EqualValues(nodeName, config.Name)
-		assert.True(config.IsBeacon)
-		assert.EqualValues(genesis, config.GenesisFile)
-		assert.EqualValues(avalancheGoConfig, config.ConfigFile)
-		assert.EqualValues(binaryPath, config.ImplSpecificConfig.(NodeConfig).BinaryPath)
-		process := &mocks.NodeProcess{}
-		process.On("Start").Return(nil)
-		return process, nil
-	}
-	net, err := NewNetwork(
-		logging.NoLog{},
-		config,
-		api.NewAPIClient,
-		newProcessF,
-	)
-	assert.NoError(err)
-	// Assert that GetNodesNames() includes only the 1 node's name
-	names, err := net.GetNodesNames()
-	assert.NoError(err)
-	assert.Contains(names, nodeName)
-	assert.Len(names, 1)
+	return networkConfig, nil
 }
