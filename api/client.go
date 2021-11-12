8971228a7229fe9cb0d605ec78a03d7f1e21cf7b
Felipe Madero <felipe.madero@gmail.com>
2021-11-12 10:35:54 -0300

    update to avalanchego 1.6.5

--- api/client.go
+++ api/client.go
@@ -15,17 +15,17 @@ import (
 // Issues API calls to a node
 // TODO: byzantine api. check if appropiate. improve implementation.
 type Client interface {
-	PChainAPI() *platformvm.Client
-	XChainAPI() *avm.Client
-	XChainWalletAPI() *avm.WalletClient
+	PChainAPI() platformvm.Client
+	XChainAPI() avm.Client
+	XChainWalletAPI() avm.WalletClient
 	CChainAPI() *evm.Client
 	CChainEthAPI() *EthClient // ethclient websocket wrapper that adds mutexed calls, and lazy conn init (on first call)
-	InfoAPI() *info.Client
-	HealthAPI() *health.Client
-	IpcsAPI() *ipcs.Client
-	KeystoreAPI() *keystore.Client
-	AdminAPI() *admin.Client
-	PChainIndexAPI() *indexer.Client
-	CChainIndexAPI() *indexer.Client
+	InfoAPI() info.Client
+	HealthAPI() health.Client
+	IpcsAPI() ipcs.Client
+	KeystoreAPI() keystore.Client
+	AdminAPI() admin.Client
+	PChainIndexAPI() indexer.Client
+	CChainIndexAPI() indexer.Client
 	// TODO add methods
 }
