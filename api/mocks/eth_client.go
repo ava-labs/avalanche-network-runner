c6d3b2b34de177963eda72524536922038c5d98a
felipemadero <felipe.madero@avalabs.org>
2021-12-17 12:21:50 -0300

    add blockbyhash to ethclient (#62)

--- api/mocks/eth_client.go
+++ api/mocks/eth_client.go
@@ -68,6 +68,29 @@ func (_m *EthClient) BalanceAt(_a0 context.Context, _a1 common.Address, _a2 *big
 	return r0, r1
 }
 
+// BlockByHash provides a mock function with given fields: _a0, _a1
+func (_m *EthClient) BlockByHash(_a0 context.Context, _a1 common.Hash) (*types.Block, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 *types.Block
+	if rf, ok := ret.Get(0).(func(context.Context, common.Hash) *types.Block); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.Block)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, common.Hash) error); ok {
+		r1 = rf(_a0, _a1)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // BlockByNumber provides a mock function with given fields: _a0, _a1
 func (_m *EthClient) BlockByNumber(_a0 context.Context, _a1 *big.Int) (*types.Block, error) {
 	ret := _m.Called(_a0, _a1)
