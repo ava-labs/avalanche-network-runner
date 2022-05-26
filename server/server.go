5ce9dec212fcaa07efaa62e3ab90ed8f661f22e2
Fabio Barone <holistic.computing@gmail.com>
2022-05-26 12:05:02 -0500

    cancel start context

--- server/server.go
+++ server/server.go
@@ -52,9 +52,10 @@ type Server interface {
 type server struct {
 	cfg Config
 
-	rootCtx   context.Context
-	closeOnce sync.Once
-	closed    chan struct{}
+	rootCtx    context.Context
+	cancelFunc context.CancelFunc // allow the Start context to be cancelled
+	closeOnce  sync.Once
+	closed     chan struct{}
 
 	ln               net.Listener
 	gRPCServer       *grpc.Server
@@ -182,10 +183,12 @@ func (s *server) Run(rootCtx context.Context) (err error) {
 		s.gRPCServer.Stop()
 		zap.L().Warn("closed gRPC server")
 		<-gRPCErrc
+		zap.L().Warn("gRPC terminated")
 
 	case err = <-gRPCErrc:
 		zap.L().Warn("gRPC server failed", zap.Error(err))
 		if !s.cfg.GwDisabled {
+			zap.L().Warn("closed gRPC gateway server", zap.Error(s.gwServer.Close()))
 			zap.L().Warn("closed gRPC gateway server", zap.Error(s.gwServer.Close()))
 			<-gwErrc
 		}
@@ -200,6 +203,9 @@ func (s *server) Run(rootCtx context.Context) (err error) {
 	if s.network != nil {
 		stopCtx, stopCtxCancel := context.WithTimeout(context.Background(), StopOnSignalTimeout)
 		defer stopCtxCancel()
+		// we need to cancel the Start context so that any pending action with that context
+		// can be cancelled
+		s.cancelFunc()
 		s.network.stop(stopCtx)
 		zap.L().Warn("network stopped")
 	}
@@ -354,9 +360,14 @@ func (s *server) Start(ctx context.Context, req *rpcpb.StartRequest) (*rpcpb.Sta
 		return nil, err
 	}
 
+	// start triggers a series of different time consuming actions
+	// (in case of subnets: create a wallet, create subnets, issue txs, etc.)
+	// We may need to cancel the context, for example if the client hits Ctrl-C
+	var cancellableCtx context.Context
+	cancellableCtx, s.cancelFunc = context.WithCancel(ctx)
 	// start non-blocking to install local cluster + custom VMs (if applicable)
 	// the user is expected to poll cluster status
-	go s.network.start(ctx)
+	go s.network.start(cancellableCtx)
 
 	// update cluster info non-blocking
 	// the user is expected to poll this latest information
