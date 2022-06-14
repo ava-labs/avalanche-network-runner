42d91fdfcebd16bc583d35fe136a589b460b84e3
Felipe Madero <felipe.madero@gmail.com>
2022-06-14 11:49:52 -0300

    fix e2e

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -219,6 +219,59 @@ var _ = ginkgo.Describe("[Start/Remove/Restart/Add/Stop]", func() {
 		}
 	})
 
+	ginkgo.It("subnet creation", func() {
+		ginkgo.By("can't ask to create 0 subnets", func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			_, err := cli.CreateSubnets(ctx, 0)
+			cancel()
+			gomega.Ω(err.Error()).Should(gomega.ContainSubstring("number of subnets to create shall be greater than 0"))
+		})
+		ginkgo.By("check subnets are 0", func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			status, err := cli.Status(ctx)
+			cancel()
+			gomega.Ω(err).Should(gomega.BeNil())
+			numSubnets := len(status.ClusterInfo.Subnets)
+			gomega.Ω(numSubnets).Should(gomega.Equal(0))
+		})
+		ginkgo.By("add 1 subnet", func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			_, err := cli.CreateSubnets(ctx, 1)
+			cancel()
+			gomega.Ω(err).Should(gomega.BeNil())
+		})
+		ginkgo.By("wait for subnets creation to finish", func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+			var created bool
+			continueLoop := true
+			for continueLoop {
+				select {
+				case <-ctx.Done():
+					continueLoop = false
+				case <-time.After(5 * time.Second):
+					ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+					status, err := cli.Status(ctx)
+					cancel()
+					gomega.Ω(err).Should(gomega.BeNil())
+					created = status.ClusterInfo.CustomVmsHealthy
+					if created {
+						continueLoop = false
+					}
+				}
+			}
+			cancel()
+			gomega.Ω(created).Should(gomega.Equal(true))
+		})
+		ginkgo.By("check subnets are 1", func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			status, err := cli.Status(ctx)
+			cancel()
+			gomega.Ω(err).Should(gomega.BeNil())
+			numSubnets := len(status.ClusterInfo.Subnets)
+			gomega.Ω(numSubnets).Should(gomega.Equal(1))
+		})
+	})
+
 	time.Sleep(10 * time.Second)
 	ginkgo.It("can remove", func() {
 		ginkgo.By("calling remove API with the first binary", func() {
@@ -356,59 +409,6 @@ var _ = ginkgo.Describe("[Start/Remove/Restart/Add/Stop]", func() {
 			}
 			cancel()
 			gomega.Ω(errCnt).Should(gomega.Equal(4))
-		})
-	})
-
-	ginkgo.It("subnet creation", func() {
-		ginkgo.By("can't ask to create 0 subnets", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-			_, err := cli.CreateSubnets(ctx, 0)
-			cancel()
-			gomega.Ω(err.Error()).Should(gomega.ContainSubstring("number of subnets to create shall be greater than 0"))
-		})
-		ginkgo.By("check subnets are 0", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-			status, err := cli.Status(ctx)
-			cancel()
-			gomega.Ω(err).Should(gomega.BeNil())
-			numSubnets := len(status.ClusterInfo.Subnets)
-			gomega.Ω(numSubnets).Should(gomega.Equal(0))
-		})
-		ginkgo.By("add 1 subnet", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-			_, err := cli.CreateSubnets(ctx, 1)
-			cancel()
-			gomega.Ω(err).Should(gomega.BeNil())
-		})
-		ginkgo.By("wait for subnets creation to finish", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
-			var created bool
-			continueLoop := true
-			for continueLoop {
-				select {
-				case <-ctx.Done():
-					continueLoop = false
-				case <-time.After(5 * time.Second):
-					ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-					status, err := cli.Status(ctx)
-					cancel()
-					gomega.Ω(err).Should(gomega.BeNil())
-					created = status.ClusterInfo.CustomVmsHealthy
-					if created {
-						continueLoop = false
-					}
-				}
-			}
-			cancel()
-			gomega.Ω(created).Should(gomega.Equal(true))
-		})
-		ginkgo.By("check subnets are 1", func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-			status, err := cli.Status(ctx)
-			cancel()
-			gomega.Ω(err).Should(gomega.BeNil())
-			numSubnets := len(status.ClusterInfo.Subnets)
-			gomega.Ω(numSubnets).Should(gomega.Equal(1))
 		})
 	})
 
