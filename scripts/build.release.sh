b3d07aab0aa0ec286529bdf2a9e04ff1cf954c85
Felipe Madero <felipe.madero@gmail.com>
2022-09-28 14:44:32 -0300

    add GCO_FLAGS setting

--- scripts/build.release.sh
+++ scripts/build.release.sh
@@ -9,18 +9,12 @@ if ! [[ "$0" =~ scripts/build.release.sh ]]; then
   exit 255
 fi
 
-if [ "${1-}" != linux ] && [ "${1-}" != darwin ]; then
-    echo "arg must be linux or darwin"
-    exit 255
-fi
-os=${1-}
-
 # https://goreleaser.com/install/
 go install -v github.com/goreleaser/goreleaser@latest
 
 # e.g.,
 # git tag 1.0.0
-#goreleaser release --config .goreleaser-$os.yml --skip-announce --skip-publish
+#goreleaser release --config .goreleaser.yml --skip-announce --skip-publish
 
 # to test without git tags
-goreleaser release --config .goreleaser-$os.yml --rm-dist --skip-announce --skip-publish --snapshot
+goreleaser release --config .goreleaser.yml --rm-dist --skip-announce --skip-publish --snapshot
