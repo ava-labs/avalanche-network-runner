db5499b337d27370c4ba0e72ef3f821e2412acaf
Felipe Madero <felipe.madero@gmail.com>
2022-10-03 17:28:20 -0300

    add comment for CGO flags setting

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,5 +15,10 @@ else
     OUTPUT=$1
 fi
 
+# Set the CGO flags to use the portable version of BLST
+#
+# We use "export" here instead of just setting a bash variable because we need
+# to pass this flag to all child processes spawned by the shell.
 export CGO_CFLAGS="-O -D__BLST_PORTABLE__"
+
 go build -v -ldflags="-X 'github.com/ava-labs/avalanche-network-runner/cmd.Version=$VERSION'" -o $OUTPUT/avalanche-network-runner
