28726e4eaec7762a64bc2982d41281fd01057e12
Felipe Madero <felipe.madero@gmail.com>
2023-04-18 07:56:48 -0300

    reenable plugins reload

--- local/blockchain.go
+++ local/blockchain.go
@@ -282,9 +282,9 @@ func (ln *localNetwork) installCustomChains(
 	}
 
 	// refresh vm list
-	//if err := ln.reloadVMPlugins(ctx); err != nil {
-	//	return nil, err
-	//}
+	if err := ln.reloadVMPlugins(ctx); err != nil {
+		return nil, err
+	}
 
 	// create blockchain from txs before spending more utxos
 	if err := ln.createBlockchains(ctx, chainSpecs, blockchainTxs, w, ln.log); err != nil {
