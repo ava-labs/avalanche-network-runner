537ec552e3e80b5717e04208b81b39d4a3cdfa27
Patrick O'Grady <prohb125@gmail.com>
2024-03-21 22:06:15 -0700

    update to AvalancheGo@v1.11.3

--- api/client.go
+++ api/client.go
@@ -4,7 +4,6 @@ import (
 	"github.com/ava-labs/avalanchego/api/admin"
 	"github.com/ava-labs/avalanchego/api/health"
 	"github.com/ava-labs/avalanchego/api/info"
-	"github.com/ava-labs/avalanchego/api/ipcs"
 	"github.com/ava-labs/avalanchego/api/keystore"
 	"github.com/ava-labs/avalanchego/indexer"
 	"github.com/ava-labs/avalanchego/vms/avm"
@@ -22,7 +21,6 @@ type Client interface {
 	CChainEthAPI() EthClient // ethclient websocket wrapper that adds mutexed calls, and lazy conn init (on first call)
 	InfoAPI() info.Client
 	HealthAPI() health.Client
-	IpcsAPI() ipcs.Client
 	KeystoreAPI() keystore.Client
 	AdminAPI() admin.Client
 	PChainIndexAPI() indexer.Client
